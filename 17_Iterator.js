791d422279167dfb7c00f0508432caa2907383d0
harpal dabhi <hdabhi902@rku.ac.in>
2024-03-08 06:42:00 +0530

    Modify iterator

--- 17_Iterator.js
+++ 17_Iterator.js
@@ -1,1 +1,13 @@
 console.log("Iterator");
+
+let school = "School of Engineering";
+
+for (const sch of school) {
+    console.log(sch);
+}
+
+const arr = [12,15,true,false,null,undefined];
+
+for (const item of arr) {
+    console.log(item);
+}
